039351c9d8a84074f483ad9d8782275b4284b0cb
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2020-06-06 15:12:01 +0700

    Updates

---
+++ precache-manifest.a8fb68262cb6e5f493d53af2ed3dea63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860de69300069d33dd1f1b4d4745c0ec",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "07aa6bd21f88c87f1de0",
+    "url": "/todo-list/static/css/main.0ef3efc8.chunk.css"
+  },
+  {
+    "revision": "d725fdf3c74f10958ecf",
+    "url": "/todo-list/static/js/2.1f2e8561.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.1f2e8561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07aa6bd21f88c87f1de0",
+    "url": "/todo-list/static/js/main.2be381b0.chunk.js"
+  },
+  {
+    "revision": "2b909f8b49d22c3e9c72",
+    "url": "/todo-list/static/js/runtime-main.5e1967fb.js"
+  }
+]);
